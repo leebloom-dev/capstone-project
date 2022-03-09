bded582f860489a359ecef43353827e8c2b41947
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-03-09 15:53:25 -0500

    started CuisineController, fragment template header, index.html template for cuisine

--- recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
+++ recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
@@ -1,4 +1,25 @@
 package org.launchcode.recipes.controllers;
 
+import org.launchcode.recipes.models.data.CuisineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cuisine")
 public class CuisineController {
+
+    @Autowired
+    private CuisineRepository cuisineRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("cuisine", cuisineRepository.findAll());
+        return "cuisine/index";
+    }
+
+
+
 }
