2c4d93833bb554f77c07788662b4cbf2a3efcde6
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-03-09 16:43:05 -0500

    created abstract entity, update fragment template, updated model and controller and cuisine template

--- recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
+++ recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
@@ -1,5 +1,6 @@
 package org.launchcode.recipes.controllers;
 
+import org.launchcode.recipes.models.Cuisine;
 import org.launchcode.recipes.models.data.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("cuisine")
+@RequestMapping("cuisines")
 public class CuisineController {
 
     @Autowired
@@ -16,10 +17,14 @@ public class CuisineController {
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("cuisine", cuisineRepository.findAll());
-        return "cuisine/index";
+        model.addAttribute("cuisines", cuisineRepository.findAll());
+        return "cuisines/index";
     }
 
-
+    @GetMapping("add")
+    public String displayAddCuisineForm(Model model) {
+        model.addAttribute(new Cuisine());
+        return "cuisines/add";
+    }
 
 }
--- recipes/src/main/java/org/launchcode/recipes/models/Cuisine.java
+++ recipes/src/main/java/org/launchcode/recipes/models/Cuisine.java
@@ -1,4 +1,7 @@
 package org.launchcode.recipes.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Cuisine {
 }
