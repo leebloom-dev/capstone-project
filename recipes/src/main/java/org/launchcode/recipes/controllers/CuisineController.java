76b920ac3f86a0ca214845850635fc241d8324c9
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-03-09 17:09:49 -0500

    Updated cuisine controller

--- recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
+++ recipes/src/main/java/org/launchcode/recipes/controllers/CuisineController.java
@@ -5,15 +5,23 @@ import org.launchcode.recipes.models.data.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("cuisines")
 public class CuisineController {
 
+    /* FIELDS */
+
     @Autowired
     private CuisineRepository cuisineRepository;
+
+
+    /* CONTROLLER METHODS */
 
     @GetMapping
     public String index(Model model) {
@@ -25,6 +33,34 @@ public class CuisineController {
     public String displayAddCuisineForm(Model model) {
         model.addAttribute(new Cuisine());
         return "cuisines/add";
+    }
+
+    @PostMapping("add")
+    public String processAddCuisineForm(@ModelAttribute @Valid Cuisine newCuisine,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Cuisine"); // added March 6
+            return "cuisines/add";
+        }
+
+        cuisineRepository.save(newCuisine);
+
+        return "redirect:";
+    }
+
+    @GetMapping("view/{cuisineId}")
+    public String displayViewCuisine(Model model, @PathVariable int cuisineId) {
+
+        Optional<Cuisine> optCuisine = cuisineRepository.findById(cuisineId);
+        if (optCuisine.isPresent()) {
+            Cuisine cuisine = (Cuisine) optCuisine.get();
+            model.addAttribute("title", "Cuisine ID: " + cuisineId);
+            model.addAttribute("cuisine", cuisine);
+            return "cuisines/view";
+        } else {
+            return "redirect:../";
+        }
     }
 
 }
