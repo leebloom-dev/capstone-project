ed7c62ef2f4a2833f3ee475086b93faac4238c15
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-03-10 15:02:11 -0500

    created models Ingredient and Recipe

--- recipes/src/main/java/org/launchcode/recipes/models/Ingredient.java
+++ recipes/src/main/java/org/launchcode/recipes/models/Ingredient.java
@@ -1,4 +1,38 @@
 package org.launchcode.recipes.models;
 
-public class Ingredient {
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Ingredient extends AbstractEntity {
+
+    /* FIELDS */
+    @ManyToMany (mappedBy = "ingredients")
+    private List<Recipe> recipes = new ArrayList<>();
+
+    @NotNull
+    @Size(max = 255, message = "Description must be between less than 255 characters")
+    private String description;
+
+
+    /* CONSTRUCTORS */
+    public Ingredient() { }
+
+
+    /* METHODS: Getters and Setters */
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
