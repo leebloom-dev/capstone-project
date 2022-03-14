a0f7c5c51aeaa975f7e9c4b3fa89825f3a897a13
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-03-14 11:06:00 -0400

    ListController and RecipeData

--- recipes/src/main/java/org/launchcode/recipes/controllers/ListController.java
+++ recipes/src/main/java/org/launchcode/recipes/controllers/ListController.java
@@ -1,12 +1,15 @@
 package org.launchcode.recipes.controllers;
 
 import org.launchcode.recipes.models.Recipe;
+import org.launchcode.recipes.models.RecipeData;
 import org.launchcode.recipes.models.data.CuisineRepository;
 import org.launchcode.recipes.models.data.IngredientRepository;
 import org.launchcode.recipes.models.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 
@@ -22,7 +25,7 @@ public class ListController {
     private IngredientRepository ingredientRepository;
 
     @Autowired
-    private RecipeRepository repository;
+    private RecipeRepository recipeRepository;
 
     static HashMap<String, String> columnChoices = new HashMap<>();
 
@@ -36,5 +39,28 @@ public class ListController {
 
 
     /* CONTROLLER METHODS */
+    @RequestMapping("")
+    public String list(Model model) {
+        model.addAttribute("cuisines", cuisineRepository.findAll());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+        return "list";
+    }
+
+    @RequestMapping(value = "recipes")
+    public String listJobsByColumnAndValue(Model model,
+                                           @RequestParam String column,
+                                           @RequestParam String value) {
+        Iterable<Recipe> recipes;
+        if (column.toLowerCase().equals("all")) {
+            recipes = recipeRepository.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            recipes = RecipeData.findByColumnAndValue(column, value, recipeRepository.findAll());
+            model.addAttribute("title", "Recipes with " + columnChoices.get(column) + ": " + value);
+        }
+        model.addAttribute("recipes", recipes);
+
+        return "list-jobs";
+    }
 
 }
--- recipes/src/main/java/org/launchcode/recipes/models/RecipeData.java
+++ recipes/src/main/java/org/launchcode/recipes/models/RecipeData.java
@@ -1,4 +1,86 @@
 package org.launchcode.recipes.models;
 
+import java.util.ArrayList;
+
 public class RecipeData {
+
+    /**
+     * Returns the results of searching the Recipes data by field and search term.
+     *
+     * For example, searching for cuisine "Japanese" will include results
+     * with "Japanese Soups".
+     *
+     * @param column Recipe field that should be searched.
+     * @param value Value of the field to search for.
+     * @param allRecipes The list of jobs to search.
+     * @return List of all recipes matching the criteria.
+     */
+    public static ArrayList<Recipe> findByColumnAndValue(String column,
+                                                         String value,
+                                                         Iterable<Recipe> allRecipes) {
+
+        ArrayList<Recipe> results = new ArrayList<>();
+
+        if (value.toLowerCase().equals("all")){
+            return (ArrayList<Recipe>) allRecipes;
+        }
+
+        if (column.equals("all")){
+            results = findByValue(value, allRecipes);
+            return results;
+        }
+        for (Recipe recipe : allRecipes) {
+
+            String aValue = getFieldValue(recipe, column);
+
+            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+                results.add(recipe);
+            }
+        }
+
+        return results;
+    }
+
+    public static String getFieldValue(Recipe recipe, String fieldName){
+        String theValue;
+        if (fieldName.equals("name")){
+            theValue = recipe.getName();
+        } else if (fieldName.equals("employer")){
+            theValue = recipe.getCuisine().toString();
+        } else {
+            theValue = recipe.getIngredients().toString();
+        }
+
+        return theValue;
+    }
+
+    /**
+     * Search all Recipe fields for the given term.
+     *
+     * @param value The search term to look for.
+     * @param allRecipes The list of jobs to search.
+     * @return      List of all jobs with at least one field containing the value.
+     */
+    public static ArrayList<Recipe> findByValue(String value,
+                                                Iterable<Recipe> allRecipes) {
+        String lower_val = value.toLowerCase();
+
+        ArrayList<Recipe> results = new ArrayList<>();
+
+        for (Recipe job : allRecipes) {
+
+            if (job.getName().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.getCuisine().toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.getIngredients().toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            }
+
+        }
+
+        return results;
+    }
 }
